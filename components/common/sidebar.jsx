940260ffef5a1efa4eefafea21f2ef9e9070b264
Rino102 <rlrino102@gmail.com>
2025-02-15 16:29:10 +0530

    patient pages, dashboard, metrics & profile

--- components/common/sidebar.jsx
+++ components/common/sidebar.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import { useRouter } from "next/router";
 
-const sidebar = () => {
+const Sidebar = () => {
+
+    const router = useRouter();
+    const { country, language } = router.query;
   return (
     <div className='sidebar-section'>
-        <a href="">Bayer Health</a>
-        <ul>
-            <li>Dashboard</li>
-            <li>My profile</li>
-            <li>Health metrics</li>
-            <li>Messages</li>
+        <h4>Bayer Health</h4>
+
+        <ul className="menuList">
+            <li onClick={() => router.push(`/${country}/${language}/bayer/patient/dashboard`)}>Dashboard</li>
+            <li onClick={() => router.push(`/${country}/${language}/bayer/patient/profile`)}>My Profile</li>
+            <li onClick={() => router.push(`/${country}/${language}/bayer/patient/metrics`)}>Health Metrics</li>
+            <li onClick={() => router.push(`/${country}/${language}/bayer/patient/messages`)}>Messages</li>
+            <li onClick={() => router.push(`/`)}>Logout</li>
         </ul>
       
     </div>
   )
 }
 
-export default sidebar
+export default Sidebar
